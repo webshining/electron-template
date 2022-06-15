cb7c3052ea347309f706f1961ec054bbe643ec4e
WebShining <79418317+webshining@users.noreply.github.com>
2022-06-15 17:14:50 +0300

    Update boot config

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,14 +1,18 @@
 const {BrowserWindow} = require('electron')
 const config = require('../config.json')
+const {updateConfig} = require('../scripts/config-edit')
 
 const bootstrap = (cAtrs) => {
-    const win = new BrowserWindow({...config, ...cAtrs})
+    const win = new BrowserWindow({...config['DWindow'], ...config['LWindow'], ...cAtrs})
     win.removeMenu()
     win.on('ready-to-show', () => {
         win.show()
     })
     win.on('close', () => {
-        
+        [width, height] = win.getSize()
+        config['LWindow'].width = width
+        config['LWindow'].height = height
+        updateConfig(config)
     })
     return win
 }
