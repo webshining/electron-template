18b78f02abb092d9ef70f643d5497f93da4878bd
artem <artemilienkono@gmail.com>
2022-07-23 20:47:31 +0300

    Update bootstrap config

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,17 +2,20 @@ const {BrowserWindow} = require('electron')
 const config = require('../config.json')
 const {updateConfig} = require('../scripts/config-edit')
 
-const bootstrap = (cAtrs) => {
-    const win = new BrowserWindow({...config['DWindow'], ...config['LWindow'], ...cAtrs})
+const bootstrap = (cAtrs={}, saveConfig=false, lastWindow=false) => {
+    
+    const win = new BrowserWindow(lastWindow ? {...config['DWindow'], ...config['LWindow'], ...cAtrs} : {...config['DWindow'], ...cAtrs})
     win.removeMenu()
     win.on('ready-to-show', () => {
         config.LWindow.maximize && win.maximize()
         win.show()
     })
     win.on('close', () => {
-        bounds = win.getBounds()
-        win.isMaximized() ? config.LWindow.maximize = true : (config.LWindow = bounds, config.LWindow.maximize = false)
-        updateConfig(config)
+        if(saveConfig){
+            bounds = win.getBounds()
+            win.isMaximized() ? config.LWindow.maximize = true : (config.LWindow = bounds, config.LWindow.maximize = false)
+            updateConfig(config)
+        }
     })
     return win
 }
