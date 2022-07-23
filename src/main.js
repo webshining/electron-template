2c97b4886264f11215344ddd0818c28b2d43600a
artem <artemilienkono@gmail.com>
2022-07-23 20:47:54 +0300

    Update main file

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 'use strict'
 const path = require('path')
-require('../scripts/reloader')(path.join(__dirname, '..'))
+require('../scripts/reloader')(path.join(__dirname, '..'), ['js'])
 
 const {app} = require('electron')
 
 const bootstrap = require('./bootstrap')
 
 app.on('ready', () => {
-    const win = bootstrap()
+    const win = bootstrap(null, true, true)
     win.loadURL('https://github.com/webshining/electron-template')
+    win.webContents.openDevTools()
 })
 
 app.on('window-all-closed', () => {
