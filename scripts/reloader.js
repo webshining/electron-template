03c72d89aed6b2ca53b8aac8e7a5888ac4fdcfa1
artem <artemilienkono@gmail.com>
2022-07-23 20:47:08 +0300

    Global update reloader

--- scripts/reloader.js
+++ scripts/reloader.js
@@ -1,26 +1,23 @@
 const { app } = require('electron')
 const fs = require('fs')
-const paths = require('path')
+
+const mainFile = /(\w+\\*\w+.\w*$)/.exec(module.parent.filename)[1]
 
 module.exports = function(path = '', fileTypes = []) {
     let windows = []
     app.on('browser-window-created', (_, bw) => {
         windows.push(bw)
     })
-    const watcher = fs.watch(path, { encoding: 'utf8', recursive: true }, (event, file) => {
-        if (paths.resolve(file) !== module.parent.filename) {
-            if (fileTypes.length === 0) {
-                windows.forEach(w => w.webContents.reloadIgnoringCache())
-            } else {
-                const reg = /\w*\.(\w*)$/
-                if (fileTypes.indexOf(reg.exec(file)) !== -1) {
-                    windows.forEach(w => w.webContents.reloadIgnoringCache())
-                }
-            }
-        } else {
-            console.log('Boot file changed, please restart app')
+    const watcher = fs.watch(path, { encoding: 'utf8', recursive: true }, (event, file=/(\w+\\*\w+.\w*$)/.exec(file)[1]) => {
+        if(mainFile == file) {
             watcher.close()
-            app.quit()
+            return app.quit()
+        }
+        if (fileTypes.length === 0) {
+            return windows.forEach(w => w.webContents.reloadIgnoringCache())
+        }
+        if(fileTypes.length > 0 && fileTypes.indexOf(/\w+.(\w+)$/.exec(file)[1]) !== -1) {
+            return windows.forEach(w => w.webContents.reloadIgnoringCache())
         }
     })
 }
