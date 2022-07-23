c1d771230f0ce41a86d77eef5f9b6689d9b5708a
WebShining <webshining@protonmail.com>
2022-07-23 15:01:30 +0300

    Update boot config

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -6,11 +6,12 @@ const bootstrap = (cAtrs) => {
     const win = new BrowserWindow({...config['DWindow'], ...config['LWindow'], ...cAtrs})
     win.removeMenu()
     win.on('ready-to-show', () => {
+        config.LWindow.maximize && win.maximize()
         win.show()
     })
     win.on('close', () => {
         bounds = win.getBounds()
-        config.LWindow = bounds
+        win.isMaximized() ? config.LWindow.maximize = true : (config.LWindow = bounds, config.LWindow.maximize = false)
         updateConfig(config)
     })
     return win
