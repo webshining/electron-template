00fd5cf9f2f75623b201e30a44f267da97a0c4ab
WebShining <webshining@protonmail.com>
2022-07-25 21:15:09 +0300

    Update boot file

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,9 +3,7 @@ const config = require('../config.json')
 const {updateConfig} = require('../scripts/config-edit')
 
 const bootstrap = (cAtrs={}, saveConfig=false, lastWindow=false) => {
-    
     const win = new BrowserWindow(lastWindow ? {...config['DWindow'], ...config['LWindow'], ...cAtrs} : {...config['DWindow'], ...cAtrs})
-    win.removeMenu()
     win.on('ready-to-show', () => {
         config.LWindow.maximize && win.maximize()
         win.show()
